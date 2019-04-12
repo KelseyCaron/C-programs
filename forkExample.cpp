ef6851591765b8d7247324a2446a12d5b97d5bf3
Kelsey Caron <kelsey.jeh@gmail.com>
2019-04-11 20:19:38 -0600

    Initial commit

---
+++ forkExample.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+	pid_t pid;
+	printf("This is really the beggining\n");
+	pid = fork();
+
+	if(pid == 0)
+	{
+		for(int i = 0; i < 10; i++)
+		{	
+			printf("I am the child with PID: %d my count is: %d\n", pid, i);
+			sleep(1);
+		}
+		
+
+	}
+	if(pid > 0)
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			printf("I am the parent with PID: %d my count is: %d\n", pid, i);
+			sleep(1);
+		}
+		
+	}
+	if(pid < 0)
+	{
+		printf("fork failed\n");
+	}
+
+  return 0;
+}
