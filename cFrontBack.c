0a8367f37e4e2ab8895eb663ce29457c73dd6099
Kelsey Caron <kelsey.jeh@gmail.com>
2019-04-16 18:48:31 -0600

    Add cFrontBack.c

---
+++ cFrontBack.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+	char x = 'X', m = 'M', l = 'L';
+	printf("Print Forwards and Backwards\n");
+	printf("Fwards: %c,%c,%c\n",x,m,l);
+	printf("Bwards: %c,%c,%c\n",l,m,x);
+
+	return 0;
+}
